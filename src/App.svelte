f86b2adb0e4d401b7fe8f04a5a3ff311141ba2bf
Robin Yang <robinyone@gmail.com>
2023-01-27 21:22:41 -0800

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,11 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
   import Fireplace from "./lib/Fireplace.svelte";
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+  <h1>Videogame Fireplace</h1>
 
   <div class="card">
     <Fireplace rowCount={64} colCount={128} />
   </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  /* .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  } */
-  .read-the-docs {
-    color: #888;
-  }
-</style>
